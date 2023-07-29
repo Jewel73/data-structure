a817ded0e8d1fe19e3b96591edd92741d2ba47fe
jewel73 <jewelranar420@gmail.com>
2023-07-29 21:48:55 +0600

    find kth position

--- src/com/array/day4/FindKthPositive.java
+++ src/com/array/day4/FindKthPositive.java
@@ -1,2 +1,30 @@
-package com.array.day4;public class FindKthPositive {
+// leetcode problem: 1539. Kth Missing Positive Number
+
+package com.array.day4;
+
+public class FindKthPositive {
+
+    public static void main(String[] args) {
+        int[] arr = {2,3,4,7,11};
+        System.out.println(findKthPositive(arr, 5));
+    }
+    public static int findKthPositive(int[] arr, int k) {
+
+        int missing = 0;
+        int checkMissing = 1;
+
+        for(int i = 0; i< arr.length ; checkMissing++){
+
+            if (checkMissing == arr[i]) {
+                i++;
+            }else{
+                missing++;
+            }
+
+            if(missing == k) return checkMissing;
+
+        }
+
+        return checkMissing + k - missing - 1;
+    }
 }
