374e367503f7ff5c91614d0d9bad38890b6e003d
jewel73 <jewelranar420@gmail.com>
2023-07-30 01:54:57 +0600

    Find kth positive number - add binary search

--- src/com/array/day4/FindKthPositive.java
+++ src/com/array/day4/FindKthPositive.java
@@ -5,26 +5,24 @@ package com.array.day4;
 public class FindKthPositive {
 
     public static void main(String[] args) {
-        int[] arr = {2,3,4,7,11};
-        System.out.println(findKthPositive(arr, 5));
+        int[] arr = {2};
+        System.out.println(findKthPositive(arr, 1));
     }
     public static int findKthPositive(int[] arr, int k) {
+       int start = 0;
+       int end = arr.length-1;
 
-        int missing = 0;
-        int checkMissing = 1;
+       while(start <= end){
+           int mid = start + (end - start)/2;
 
-        for(int i = 0; i< arr.length ; checkMissing++){
+           if(arr[mid] - (mid+1) >= k){
+               end = mid - 1;
 
-            if (checkMissing == arr[i]) {
-                i++;
-            }else{
-                missing++;
-            }
+           }else {
+               start = mid +1;
+           }
+       }
 
-            if(missing == k) return checkMissing;
-
-        }
-
-        return checkMissing + k - missing - 1;
+       return start + k;
     }
 }
